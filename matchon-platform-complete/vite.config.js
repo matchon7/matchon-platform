fb345e89965b3460a9eac6c57a8ffd2d8c212ba5
꼴통티비 <matchon7@daum.net>
2026-08-12 18:44:44 +0900

    matchon.kr 연결을 위한 Vite 기본 경로 수정
    Changed the base path for the Vite configuration.

--- matchon-platform-complete/vite.config.js
+++ matchon-platform-complete/vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/matchon-platform/",
+  base: "/",
   plugins: [react()],
-  build: {
-    outDir: "dist",
-    assetsDir: "assets",
-    emptyOutDir: true
-  }
 });
